d6d913351a4581dac34477b7de122304aecd341c
Paul <paul.dashkevich@yandex.ru>
2023-05-25 00:27:59 +0300

    feat: update user auth with phone

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,32 +1,74 @@
 import React, { ChangeEvent, FormEventHandler, useState } from 'react';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import {
+  ConfirmationResult,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+  updateProfile,
+} from 'firebase/auth';
 import { ButtonWrapper, Form, FormTitle, Input, RecaptchaContainer, SignUpWrapper } from './styled';
 import Button from '../Button/Button';
 import { auth } from '../../../firebase';
 
+declare const window: Window &
+  typeof globalThis & {
+    recaptchaVerifier: RecaptchaVerifier;
+    confirmationResult: ConfirmationResult;
+  };
+
+const generateRecaptcha = () => {
+  window.recaptchaVerifier = new RecaptchaVerifier(
+    'recaptcha-container',
+    {
+      size: 'invisible',
+      callback: () => {},
+    },
+    auth
+  );
+};
+
 const SignUp = () => {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [phone, setPhone] = useState<string>('');
+  const [OTP, setOTP] = useState<string>('');
+  const [isRequested, setRequested] = useState<boolean>(false);
 
   const onSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
+    generateRecaptcha();
+    const appVerifier = window.recaptchaVerifier;
+    setRequested(true);
 
-    await createUserWithEmailAndPassword(auth, email, password)
-      .then((userCred) => {
-        const user = userCred.user;
-        console.log(user);
+    await signInWithPhoneNumber(auth, phone, appVerifier)
+      .then((confirmationResult: ConfirmationResult) => {
+        window.confirmationResult = confirmationResult;
       })
-      .catch((error) => console.error(error));
+      .catch(() => {});
   };
 
-  const handleChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    setEmail(value);
+    setName(value);
   };
 
-  const handleChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+  const handlePhoneChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    setPassword(value);
+    setPhone(value);
+  };
+
+  const verifyOTP = (e: ChangeEvent<HTMLInputElement>) => {
+    const otp = e.target.value;
+    setOTP(otp);
+
+    if (otp.length === 6) {
+      const confirmationResult = window.confirmationResult;
+      confirmationResult
+        .confirm(otp)
+        .then((result) => {
+          const user = result.user;
+          updateProfile(user, { displayName: name }).catch(() => {});
+        })
+        .catch(() => {});
+    }
   };
 
   return (
@@ -34,17 +76,11 @@ const SignUp = () => {
       <RecaptchaContainer id="recaptcha-container" />
       <Form onSubmit={onSubmit}>
         <FormTitle>Create an account</FormTitle>
-        <Input type="text" placeholder="Email" value={email} onChange={handleChangeEmail} />
-        <Input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={handleChangePassword}
-        />
+        <Input type="text" placeholder="Name" value={name} onChange={handleNameChange} />
+        <Input type="text" placeholder="Phone number" value={phone} onChange={handlePhoneChange} />
+        {isRequested && <Input type="text" placeholder="OTP" value={OTP} onChange={verifyOTP} />}
         <ButtonWrapper>
-          <Button data-id="sign-up-button" buttonType="primary">
-            Next
-          </Button>
+          <Button buttonType="primary">Next</Button>
         </ButtonWrapper>
       </Form>
     </SignUpWrapper>
